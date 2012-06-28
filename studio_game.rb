9edd55296e8d47a6b4aa02721d8469e7dc764072
RayHightower <rth@wisdomgroup.com>
2012-06-28 18:55:55 -0500

    more experiments

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,22 @@
+puts "\n**** Let's keep 'em separated. ****\n\n"
 player1 = "larry"
 player2 = "curly"
 player3 = "moe"
+player4 = "shemp"
+
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
+
+puts "#{player1.capitalize} has a health of #{health1}."
+puts "#{player2.upcase} has a health of #{health2}."
+puts "#{player3.capitalize} has a health of #{health3}.".center(50,'*')
+puts "#{player4.capitalize.ljust(40,'.')} #{health4} health"
+
+puts "\n**** Let's keep 'em separated. ****\n"
+
+
 
 player = %w["larry", "curly", "moe"]
 health = %w[60, 85, 129]
