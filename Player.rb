6f12481cd3db971c6ed181c42723793ef369eb7d
RayHightower <rth@wisdomgroup.com>
2012-06-29 16:59:50 -0500

    added health and score

--- Player.rb
+++ Player.rb
@@ -1,12 +1,16 @@
 class Player
 
+  # attr_reader :name, :health
+  # attr_writer :name, :health
+  attr_accessor :name, :health
+  
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    return "\nI'm #{@name} with a health of #{@health}. Object_ID #{self.object_id}."
+    return "\nI'm #{@name} with a health of #{@health} and a score of #{@score}."
   end
 
   def w00t
@@ -17,6 +21,10 @@ class Player
   def blam
     puts "\n#{@name} got blammed!"
     @health -= 10
+  end
+
+  def score
+    @score = @health + @name.length
   end
 
 end
@@ -37,3 +45,12 @@ puts player3
 player3.w00t
 puts player3
 
+puts "\n\nBegin Exercise Eight ********"
+
+player2 = Player.new("larry", 60)
+puts player2.name
+player2.name = "Lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
+puts player2
