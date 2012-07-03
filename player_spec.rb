bb844dcf7d2e71d758b7651caedef2034b904f9e
RayHightower <rth@wisdomgroup.com>
2012-07-02 23:09:19 -0400

    --format doc in .rspec

--- player_spec.rb
+++ player_spec.rb
@@ -4,6 +4,7 @@ describe Player do
 
   before do
     @player = Player.new("geronimo", 200)
+    @initial_health = @player.health
   end
 
   it "has a capitalized name" do
@@ -19,12 +20,21 @@ describe Player do
   end
 
   it "computes a score as the sum of its health and length of name" do
+    @player.score.should == @player.health + @player.name.length
   end
 
   it "increases health by 15 when w00ted" do
+    w00ted_health = @initial_health + 15
+    @player.w00t
+    @player.health.should == w00ted_health
   end
 
   it "decreases health by 10 when blammed" do
+    blammed_health = @initial_health - 10
+    @player.blam
+    @player.health.should == blammed_health
   end
+
+  it "should slice and dice"
 
 end
