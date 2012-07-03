0fc94265778da7826fa4edf71944f87e83de3807
RayHightower <rth@wisdomgroup.com>
2012-07-03 19:33:23 -0400

    using Spinner instead of Die

--- game_spec.rb
+++ game_spec.rb
@@ -1,8 +1,15 @@
+# instead of Die and roll, use Spinner and spin
+#
 require_relative './game'
 
 describe Game do
   before do
     @game = Game.new("streetracer")
+
+    @initiall_health = 100
+    @player = Player.new("moe", @initial_health)
+
+    @game.add_player(@player)
   end
 
   it "knows how to initialize itself"
