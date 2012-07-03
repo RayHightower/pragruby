05968cce7bad84261c844750d5c78dc8d5c7eac6
RayHightower <rth@wisdomgroup.com>
2012-07-02 22:53:16 -0400

    adding tests

--- player_spec.rb
+++ player_spec.rb
@@ -15,6 +15,7 @@ describe Player do
   end
 
   it "has a string representation" do
+    @player.to_s.should == "I'm Geronimo with a health of 200 and a 208 score."
   end
 
   it "computes a score as the sum of its health and length of name" do
