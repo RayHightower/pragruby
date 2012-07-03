fb2b30c03074da9755c5d3318778f6d16db78aa8
RayHightower <rth@wisdomgroup.com>
2012-07-02 22:50:15 -0400

    added rspec and guard

--- player_spec.rb
+++ player_spec.rb
@@ -1,3 +1,29 @@
 require_relative './player'
 
+describe Player do
 
+  before do
+    @player = Player.new("geronimo", 200)
+  end
+
+  it "has a capitalized name" do
+    @player.name.should == "Geronimo"
+  end
+
+  it "has an inital health" do
+    @player.health.should_not == nil
+  end
+
+  it "has a string representation" do
+  end
+
+  it "computes a score as the sum of its health and length of name" do
+  end
+
+  it "increases health by 15 when w00ted" do
+  end
+
+  it "decreases health by 10 when blammed" do
+  end
+
+end
