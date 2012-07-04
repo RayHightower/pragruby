f0cf57a433f227aabed2645809c1420309e6807d
RayHightower <rth@wisdomgroup.com>
2012-07-04 19:30:37 -0400

    adding spinner tests

--- spinner.rb
+++ spinner.rb
@@ -1,3 +1,7 @@
 class Spinner
 
+  def roll
+    5
+  end
+
 end
--- spinner_spec.rb
+++ spinner_spec.rb
@@ -5,6 +5,12 @@ describe Spinner do
     @spinner = Spinner.new
   end
 
-  it 'should create a spinner of six possible values'
+  it 'should roll an integer greater than or equal to one' do
+    @spinner.roll.should >= 1
+  end
+
+  it 'should roll an integer less than or equal to six' do
+    @spinner.roll.should <= 6
+  end
 
 end
