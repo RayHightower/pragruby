d1cde83bbb926f5ea3f482f3ac4ff8d75ceb9e1c
RayHightower <rth@wisdomgroup.com>
2012-07-05 15:48:17 -0400

    added Struct and constant TREASURES

--- treasure_trove.rb
+++ treasure_trove.rb
@@ -1,3 +1,12 @@
 module TreasureTrove
 
+  Treasure = Struct.new(:name, :points)
+
+  TREASURES = [['pie', 5],
+               ['bottle', 25],
+               ['hammer', 50],
+               ['skillet', 100],
+               ['broomstick', 200],
+               ['crowbar', 400]]
+
 end
