ae8d1fc72653837bd6057b0d73cc5f87c7279002
RayHightower <rth@wisdomgroup.com>
2012-07-05 17:09:04 -0400

    random treasure found. all stests passing

--- game_turn.rb
+++ game_turn.rb
@@ -12,5 +12,11 @@ module GameTurn
     else
       puts "\n*** #{player.name} was skipped."
     end
+    treasure = TreasureTrove::TREASURES.sample
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
+  end
+
+  def get_treasure
+    @treasure = TreasureTrove::TREASURES.sample
   end
 end
