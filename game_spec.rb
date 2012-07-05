37749082a07ff2bb9f8f353a543529bd86478472
RayHightower <rth@wisdomgroup.com>
2012-07-05 10:45:34 -0400

    change game_spec.rb to allow for multiple rounds in the game

--- game_spec.rb
+++ game_spec.rb
@@ -10,26 +10,27 @@ describe Game do
 
       @initial_health = 100
       @player = Player.new("moe", @initial_health)
+      @number_of_rounds = 2
 
       @game.add_player(@player)
     end
 
-    it "w00ts the player when the number is high" do
+    it "w00ts the player when a high number is spun" do
       @spinner = Spinner.any_instance.stub(:spin).and_return(5)
-      @game.play
-      @player.health.should == @initial_health + 15
+      @game.play(@number_of_rounds)
+      @player.health.should == @initial_health + (15 * @number_of_rounds)
     end
 
-    it "does nothing when the number is medium" do
+    it "does nothing when a medium number is spun" do
       @spinner = Spinner.any_instance.stub(:spin).and_return(4)
-      @game.play
+      @game.play(@number_of_rounds)
       @player.health.should == @initial_health
     end
 
-    it "blams the player when the number is low" do
+    it "blams the player when a low number is spun" do
       @spinner = Spinner.any_instance.stub(:spin).and_return(2)
-      @game.play
-      @player.health.should == @initial_health - 10
+      @game.play(@number_of_rounds)
+      @player.health.should == @initial_health - (10 * @number_of_rounds)
     end
   end
 end
