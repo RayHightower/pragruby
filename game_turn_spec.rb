7b12894792d06cacdef729b446c3f7fe6f838811
RayHightower <rth@wisdomgroup.com>
2012-07-12 22:33:37 -0500

    gotta figure out the best way to test this

--- game_turn_spec.rb
+++ game_turn_spec.rb
@@ -1,11 +1,20 @@
 require_relative './game_turn.rb'
+
 # Is it overkill to have this file when other files are covering the GameTurn module with tests?
 #
 
 describe GameTurn do
 
+  before do
+    @player = Player.new("Kirk",100)
+  end
+
+  it 'should take a turn' do
+    GameTurn::take_turn(@player).should_not == nil
+  end
+
   it 'should grab a treasure item' do
-    treasure = get_treasure
+    treasure = GameTurn::get_treasure
     treasure.should_not == nil
   end
 
