7f992afa1c74ef1f301c867b85f82888d849641b
RayHightower <rth@wisdomgroup.com>
2012-07-21 01:23:22 -0500

    starting bonus round

--- studio_game.rb
+++ studio_game.rb
@@ -9,7 +9,11 @@ knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
-knuckleheads.play(10)
+
+knuckleheads.play(10) do
+  knuckleheads.total_points >= 2000
+end
+
 
 knuckleheads.show_game_status
 knuckleheads.print_stats
@@ -28,3 +32,4 @@ knuckleheads.print_high_scores
 # chipmunks.show_game_status
 # chipmunks.print_stats
 # chipmunks.print_high_scores
+
