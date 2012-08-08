a13d230468e59681c537240afe4715aa03acfb1f
Raymond T. Hightower <rth@wisdomgroup.com>
2012-08-08 17:38:40 -0500

    putting classes & modules into StudioGame module

--- playable.rb
+++ playable.rb
@@ -1,17 +1,20 @@
-module Playable
-  
-  def w00t
-    puts "\n#{name} got w00ted!"
-    self.health += 15
-  end
+module StudioGame
 
-  def blam
-    puts "\n#{name} got blammed!"
-    self.health -= 10
-  end
+  module Playable
+    
+    def w00t
+      puts "\n#{name} got w00ted!"
+      self.health += 15
+    end
 
-  def strong?  # RSpec allows some syntactic sugar w/predicate methods.
-    self.health > 100    
-  end
+    def blam
+      puts "\n#{name} got blammed!"
+      self.health -= 10
+    end
 
+    def strong?  # RSpec allows some syntactic sugar w/predicate methods.
+      self.health > 100    
+    end
+
+  end
 end
