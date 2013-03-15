0829e87753725994909c7f3b0fbf8ceaa947a341
Raymond T. Hightower <rth@wisdomgroup.com>
2013-03-14 19:46:22 -0500

    simple stuff

--- 2013-03/studio.rb
+++ 2013-03/studio.rb
@@ -1,6 +1,13 @@
 player1 = "larry"
+player2 = "curly"
+player3 = "moe"
 health1 = 60
 
 puts "#{player1}'s health is #{health1}."
 puts "#{player1}'s triple-health is #{3*health1}."
+
+puts "Players:"
+puts "\t#{player1}"
+puts "\t#{player2}"
+puts "\t#{player3}"
 
