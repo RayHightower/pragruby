598c98ecfa3d49d7aa67df269312dacd547341bf
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-28 15:19:23 -0600

    Cleanup clumsly player spec.

--- spec/studio_game/clumsy_player_spec.rb
+++ spec/studio_game/clumsy_player_spec.rb
@@ -6,7 +6,7 @@ module StudioGame
     before do
       @player = ClumsyPlayer.new("klutz",100,10)
     end
-    
+
     it "only gets half the point value for each treasure" do
       @player.points.should == 0
 
@@ -19,15 +19,15 @@ module StudioGame
 
       crowbar = Treasure.new(:crowbar, 400)
       @player.found_treasure(crowbar)
-      
-      @player.points.should == 275    
-      
+
+      @player.points.should == 275
+
       yielded = []
       @player.each_found_treasure do |treasure|
         yielded << treasure
       end
 
-      yielded.should == [Treasure.new(:hammer, 75), Treasure.new(:crowbar, 200)]    
+      yielded.should == [Treasure.new(:hammer, 75), Treasure.new(:crowbar, 200)]
     end
 
     it "applies the boost factor as expected" do
